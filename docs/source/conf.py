a8ad77d0b30939261085dd20e197d60c3392dff4
cold-young <coldyoung38@gmail.com>
2024-08-06 17:57:22 +0900

    Test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,3 @@
-import sphinx_rtd_theme
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -16,21 +15,17 @@ release = '0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx_rtd_theme',
-    'myst-parser'
+    'sphinx_rtd_theme'
 ]
-source_suffix = {
-    '.rst' : 'restructuredtext',
-    '.txt' : 'markdown',
-    '.md' : 'markdown',
-    
-}
 
 templates_path = ['_templates']
-exclude_patterns = ['build']
+exclude_patterns = []
+
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = ['_themes']
 html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. xccels_ReadtheDocs documentation master file, created by
-   sphinx-quickstart on Tue Aug  6 16:13:12 2024.
+   sphinx-quickstart on Tue Aug  6 17:53:29 2024.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
